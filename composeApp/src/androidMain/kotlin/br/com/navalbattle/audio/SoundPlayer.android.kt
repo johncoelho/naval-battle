67ead56ac62f02de231fcfbf6eb5dd393ea8cb65
johncoelho <joncqs@gmail.com>
2026-09-12 19:24:20 -0300

    Add files via upload

---
+++ composeApp/src/androidMain/kotlin/br/com/navalbattle/audio/SoundPlayer.android.kt
@@ -0,0 +1,39 @@
+package br.com.navalbattle.audio
+
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.SoundPool
+import br.com.navalbattle.R
+
+/** Contexto de aplicação registrado por [br.com.navalbattle.MainActivity] na inicialização. */
+object AudioContextHolder {
+    lateinit var appContext: Context
+}
+
+actual class SoundPlayer actual constructor() {
+    private val pool: SoundPool = SoundPool.Builder()
+        .setMaxStreams(4)
+        .setAudioAttributes(
+            AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build()
+        )
+        .build()
+
+    private val ids: Map<Sfx, Int> = mapOf(
+        Sfx.MISS to pool.load(AudioContextHolder.appContext, R.raw.sfx_miss, 1),
+        Sfx.HIT to pool.load(AudioContextHolder.appContext, R.raw.sfx_hit, 1),
+        Sfx.SUNK to pool.load(AudioContextHolder.appContext, R.raw.sfx_sunk, 1)
+    )
+
+    actual fun play(sfx: Sfx) {
+        val id = ids[sfx] ?: return
+        val volume = if (sfx == Sfx.SUNK) 1f else 0.85f
+        pool.play(id, volume, volume, 1, 0, 1f)
+    }
+
+    actual fun release() {
+        pool.release()
+    }
+}
