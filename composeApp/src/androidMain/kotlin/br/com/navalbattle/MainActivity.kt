6c12b8c33d936496dd3b8b3eafac28386b652590
johncoelho <joncqs@gmail.com>
2026-09-12 19:15:48 -0300

    Add files via upload

--- composeApp/src/androidMain/kotlin/br/com/navalbattle/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/br/com/navalbattle/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import br.com.navalbattle.audio.AudioContextHolder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        AudioContextHolder.appContext = applicationContext
         setContent { App() }
     }
 }
