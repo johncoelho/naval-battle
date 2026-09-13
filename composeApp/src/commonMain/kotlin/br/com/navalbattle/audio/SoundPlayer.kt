167671073bd9d05a45d92c8f738602522aa2e969
johncoelho <joncqs@gmail.com>
2026-09-12 21:12:46 -0300

    Add files via upload

--- composeApp/src/commonMain/kotlin/br/com/navalbattle/audio/SoundPlayer.kt
+++ composeApp/src/commonMain/kotlin/br/com/navalbattle/audio/SoundPlayer.kt
@@ -1,6 +1,17 @@
 package br.com.navalbattle.audio
 
-enum class Sfx { LAUNCH, MISS, HIT, SUNK }
+enum class Sfx {
+    LAUNCH,
+    MISS,
+    HIT,
+    SUNK,
+
+    /** Alarme de colisão: soa só quando a SUA frota é atingida. */
+    ALARM,
+
+    /** Klaxon de emergência: soa quando você perde um navio. */
+    ALARM_CRITICAL
+}
 
 /** Toca efeitos sonoros curtos de combate. Implementação nativa por plataforma. */
 expect class SoundPlayer() {
